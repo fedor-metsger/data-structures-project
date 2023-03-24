6e1bc9f6b79219220b4c0c7816ce2c87af73eb48
Fedor Metsger <fedor.metsger@gmail.com>
2023-03-24 21:46:34 +0300

    Add homework #5

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,34 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data, next_node):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        """
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
-
+    def __init__(self):
+        """Конструктор класса LinkedList"""
+        self.head = self.tail = None
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        if self.head:
+            self.head = Node(data, self.head)
+        else:
+            self.head = self.tail = Node(data, None)
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        if self.tail:
+            self.tail.next_node = Node(data, None)
+            self.tail = self.tail.next_node
+        else:
+            self.head = self.tail = Node(data, None)
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,8 +38,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,45 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+
+import unittest
+from src.linked_list import LinkedList, Node
+
+class TestLinkedList(unittest.TestCase):
+    """
+    Класс для тестирования класса Stack
+    """
+    def test_init(self):
+        """Тестирует метод Stack.__init__()"""
+        llist = LinkedList()
+        self.assertEqual(llist.head, None)
+        self.assertEqual(llist.tail, None)
+
+    def test_insert_head(self):
+        llist = LinkedList()
+        llist.insert_beginning("data1")
+        self.assertEqual(llist.head.data, "data1")
+        self.assertEqual(llist.tail.data, "data1")
+        llist.insert_beginning("data2")
+        self.assertEqual(llist.head.data, "data2")
+        self.assertEqual(llist.tail.data, "data1")
+
+    def test_insert_tail(self):
+        llist = LinkedList()
+        llist.insert_at_end("data1")
+        self.assertEqual(llist.head.data, "data1")
+        self.assertEqual(llist.tail.data, "data1")
+        llist.insert_at_end("data2")
+        self.assertEqual(llist.head.data, "data1")
+        self.assertEqual(llist.tail.data, "data2")
+
+    def test_str(self):
+        llist = LinkedList()
+        self.assertEqual(llist.__str__(), "None")
+        llist.insert_beginning("data1")
+        llist.insert_beginning("data2")
+        llist.insert_beginning("data3")
+        self.assertEqual(llist.__str__(), "data3 -> data2 -> data1 -> None")
+
+    def test_node(self):
+        node = Node('data1', None)
+        self.assertEqual(node.data, 'data1')
+        self.assertEqual(node.next_node, None)
